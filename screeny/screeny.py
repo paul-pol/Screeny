1612d7d3016062465312195867ae4d298169a203
Paul Pol <mail@paul-pol.de>
2023-05-09 16:52:55 +0200

    - Implemented new function "get_mouse_pos". - changed some variable-names for better readability

--- screeny/screeny.py
+++ screeny/screeny.py
@@ -1,7 +1,10 @@
 from PySide6.QtCore import QRect
+from PySide6.QtGui import QGuiApplication
 
 import numpy as np
 import mss as m
+
+from screeny.mouse import Mouse
 
 
 class Screeny:
@@ -11,17 +14,28 @@ class Screeny:
         Initializing variables of the class.
         """
         self.mss = m.mss()
-        self.monitor = self.mss.monitors[1]
+        self.mss_monitor = self.mss.monitors[1]
+        self.q_screen = QGuiApplication().primaryScreen()
+        self.mouse = Mouse(self.q_screen)
 
     def take_screenshot(self, rect: QRect = None):
         """
-        Makes a screenshot of the complete monitor or a given area.
+        Takes a screenshot of the complete monitor or a given area.
 
         :param rect:    Rectangular area where the screenshot will be taken.
         :return:        Image as a numpy-array.
         """
-        if rect == None:
-            img = np.array(self.mss.grab(self.monitor))
+        if rect is None:
+            img = np.array(self.mss.grab(self.mss_monitor))
         else:
             img = np.array(self.mss.grab((rect.x(), rect.y(), rect.width(), rect.height())))
         return img
+
+    def get_mouse_pos(self):
+        """
+        Returns the current position of the mouse.
+
+        :return:    Tuple of xy-coordinates of the current mouse position. -> (x, y)
+        """
+        return self.mouse.get_pos()
+
